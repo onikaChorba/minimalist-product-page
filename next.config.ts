db10e7c57643a176366293a9ed8b26be2ad11592
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2024-10-28 13:28:49 +0200

    add basePath, trailingSlash

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  basePath: '/minimalist-product-page',
+  trailingSlash: true,
   reactStrictMode: true,
   images: {
     domains: ['cdn.dummyjson.com'],
